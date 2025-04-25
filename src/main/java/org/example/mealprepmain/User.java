b0894ad1b72a5ce6c1cce4a9adbefb21e7c6ebbc
Shameer-R <roseshammy@gmail.com>
2025-04-25 17:24:04 -0400

    Add password as field to user class

--- src/main/java/org/example/mealprepmain/User.java
+++ src/main/java/org/example/mealprepmain/User.java
@@ -6,13 +6,15 @@ public class User {
     private String firstName;
     private String lastName;
     private String email;
+    private String password;
     private String birthday;
     private List<String> preferences; //food preferences
 
-    public User(String firstName, String lastName, String email, String birthday, List<String> preferences){
+    public User(String firstName, String lastName, String email, String password, String birthday, List<String> preferences){
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.password = password;
         this.birthday = birthday;
         this.preferences = preferences;
     }
@@ -26,6 +28,8 @@ public class User {
     public String getEmail() {
         return email;
     }
+
+    public String getPassword() {return password;}
     public String getBirthday() {
         return birthday;
     }
